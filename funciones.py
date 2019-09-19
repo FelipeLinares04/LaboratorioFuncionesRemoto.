13487ac906588b44c5b9a458e2be95edab1ee4ed
Felipe Linares <Carlos.Linares.2019@upb.edu.co>
2019-09-19 09:52:53 -0500

    primero

---
+++ funciones.py
@@ -0,0 +1,12 @@
+def a_power_b(a,b):
+    x=1
+    for i in range(b):
+        x=x*a
+    return x
+
+
+n1 = int(input("dijite primer numero"))
+n2 = int(input("dijite exponente"))
+result=a_power_b(n1,n2)
+print(result)
+
